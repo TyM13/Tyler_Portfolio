1493d3f15b28bcef5c51477da4be36714a8c83eb
Your Name <you@example.com>
2023-02-02 19:28:17 -0700

    portfolio

---
+++ src/components/MainPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <page-header></page-header>
+    <section>
+      <h2>test</h2>
+      <h1>title</h1>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
